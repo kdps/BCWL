328c5b4eccca668430092fd5f9767790f7b677c0
kdp <torinoyume@naver.com>
2019-08-30 00:31:41 +0900

    Update bcwl.c

--- bcwl.c
+++ bcwl.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <pwd.h>
 
+#define SERVER_ADDR     "127.0.0.1"
+#define MAXBUF          1024
+
+'Internet Protocol => AF_INET : Address, PF_INET : Protocol
 
 int main(int argc, char *argv[]) {
 
